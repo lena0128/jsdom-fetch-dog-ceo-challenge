fdcc712132e5a83299ca8b9cfc85b6c8a299dcee
Lena Hou <lenahou2013@gmail.com>
2021-09-24 21:40:30 -0400

    commit tree 83d53012882bee85a7ded7cc11525eff4d3759c9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetchDogImage();
+    fetchBreeds();
+    filterNames(); 
+})
+
+function fetchDogImage() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(r => r.json())
+    .then(data => renderImages(data));
+}
+
+function fetchBreeds() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(r => r.json())
+    .then(json => renderBreed(json));
+}
+
+function renderImages(data) {
+    data.message.forEach(function(oneImage){
+      const image = document.createElement('img');
+      const ul = document.querySelector('#dog-image-container');
+      image.src = oneImage;
+      ul.append(image);
+    })
+  }
+
+function renderBreed(json) {
+    for (let breedLink in json.message ) {
+      const breed = document.createElement('li');
+      const ul = document.querySelector('#dog-breeds');
+  
+  
+      if (json.message[breedLink].length === 0) {
+        const breed = document.createElement('li');
+        breed.className = 'links';
+        breed.innerText = breedLink;
+        breed.addEventListener('click', function(){
+          breed.style.color = '#FBBF29';
+        });
+        ul.append(breed);
+      } else {
+        json.message[breedLink].forEach(function(innerBreed){
+          const breed = document.createElement('li');
+          breed.className = 'links';
+          breed.innerText = `${innerBreed} ${breedLink}`;
+          breed.addEventListener('click', function(){
+            breed.style.color = '#FBBF29';
+          });
+          ul.append(breed);
+        })
+      }
+    }
+  }
+
+
+  function filterNames() {
+    const dropDown = document.querySelector('#breed-dropdown');
+    dropDown.addEventListener('change', function(e){
+        const listOfAll = document.querySelectorAll('li')
+        listOfAll.forEach(function(breed){
+          if (dropDown.value === breed.innerText.charAt(0) ) {
+            breed.style.color = 'red'
+            breed.style.display = 'block'
+          } else {
+            breed.style.display = 'none'
+          }
+        })
+      })
+  }
